42272a55638d77023a371e2aca89abaa5400054c
omar <omarcornut@gmail.com>
2020-05-22 16:48:13 +0200

    Tables: Resizing weighted column preserve sum of weights. Fix ResizedColumn init leading to undesirable TableSetColumnWidth() on first run. Rework TableSettingsHandler_ReadLine() structure to allow other types of line.

--- imgui_internal.h
+++ imgui_internal.h
@@ -2001,6 +2001,7 @@ struct ImGuiTable
         LastResizedColumn = -1;
         ContextPopupColumn = -1;
         ReorderColumn = -1;
+        ResizedColumn = -1;
     }
 };
 
@@ -2012,7 +2013,7 @@ struct ImGuiTableColumnSettings
     ImS8    Index;
     ImS8    DisplayOrder;
     ImS8    SortOrder;
-    ImS8    SortDirection : 7;
+    ImU8    SortDirection : 2;
     ImU8    Visible : 1;
 
     ImGuiTableColumnSettings()
